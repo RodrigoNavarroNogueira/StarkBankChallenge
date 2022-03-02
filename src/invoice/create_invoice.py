e695ee94ada561313bbb4ca2442859f31c3c4a1d
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-03-02 12:49:01 -0300

    script to create invoices

---
+++ src/invoice/create_invoice.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+import starkbank
+
+from src.authentication import user
+from src.invoice.data_invoice_generator import random_amount, random_name, cpf
+
+starkbank.user = user
+
+
+def create_invoice():
+    invoices = starkbank.invoice.create([
+        starkbank.Invoice(
+            amount=random_amount(),
+            descriptions=[{'key': 'Arya', 'value': 'Not today'}],
+            discounts=[{'percentage': 10, 'due': datetime.now()+timedelta(days=10)}],
+            due=datetime.now()+timedelta(days=10),
+            expiration=123456789,
+            fine=2.5,
+            interest=1.3,
+            name=random_name(),
+            tags=['New sword', 'Invoice #1234'],
+            tax_id=cpf()
+        )
+    ])
+    return invoices
+
+create_invoice()
