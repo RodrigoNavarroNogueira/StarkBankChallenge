7c3d92eee9de0604ec72b488150038c52ae52440
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-03-04 19:50:59 -0300

    updating constants with invoice id

--- src/invoice/get_invoice.py
+++ src/invoice/get_invoice.py
@@ -1,10 +1,11 @@
 import starkbank
 
 from src.authentication import user
+from src.utils.constants import INVOICE_ID
 
 starkbank.user = user
 
-id_invoice = '4659866144604160'
+id_invoice = INVOICE_ID
 invoice = starkbank.invoice.get(id_invoice)
 
 print(invoice)
