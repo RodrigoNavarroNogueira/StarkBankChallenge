0ac9b5f43d5edb3c2db9a336ec0d326d469fe327
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-03-07 15:29:09 -0300

    adding log for log transfer query

--- src/transfer/get_transfer_log.py
+++ src/transfer/get_transfer_log.py
@@ -1,9 +1,14 @@
+import logging
+
 import starkbank
 
 from src.authentication import user
+from src.utils.constants import TRANSFER_LOG
 
 starkbank.user = user
 
-log = starkbank.transfer.log.get("4571465684877312")
+logging.getLogger().setLevel(logging.INFO)
 
-print(log)
+log = starkbank.transfer.log.get(TRANSFER_LOG)
+
+logging.info(f'Looking for transfer with log number...\n{log}')
