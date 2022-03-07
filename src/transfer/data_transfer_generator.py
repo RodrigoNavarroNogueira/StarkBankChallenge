cb95f388af6410bcf60ff4c56665f57a05233b09
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-03-07 15:30:58 -0300

    fixing function to create an external_id

--- src/transfer/data_transfer_generator.py
+++ src/transfer/data_transfer_generator.py
@@ -6,7 +6,13 @@ from src.authentication import user
 
 starkbank.user = user
 
+lis = []
+
 
 def create_external_id():
     for num in range(15):
-        print(random.randrange(10), end="")
+        id = str(random.randrange(10))
+        lis.append(id)
+    num = ""
+    external_id = num.join(lis) 
+    return external_id
