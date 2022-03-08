5ec9eb27e2d16977f3e206b3e40f186b17d1e795
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-03-08 10:03:25 -0300

    adding log

--- src/invoice/list_invoice_log.py
+++ src/invoice/list_invoice_log.py
@@ -10,10 +10,10 @@ starkbank.user = user
 logging.getLogger().setLevel(logging.INFO)
 
 logs = starkbank.invoice.log.query(
-    after="2020-01-01",
+    after="2022-03-07",
     before=datetime.now(),
     limit=10
 )
 
 for log in logs:
-    logging.info(f'Consulting invoice\n{log}')
+    logging.info(f'Consulting invoice...\n{log}')
